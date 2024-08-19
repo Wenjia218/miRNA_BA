9d6b08955884e60e543b0257a9c2027d1ef519ef
Wenjia <zhongwenjia218@gmail.com>
2024-08-19 13:26:52 +0200

    commit

--- Info_specie.py
+++ Info_specie.py
@@ -3,7 +3,7 @@ import json
 plant_papers = {}
 list_of_plants = []
 
-filename = 'myresults/db_plant.txt'
+filename = 'my_results/db_mirplant.txt'
 f = open(filename, encoding="utf-8")
 lines = f.readlines()
 for line in lines:
@@ -16,7 +16,7 @@ for name in plant_papers:
 
 
 plant_mir = {}
-with open('myresults/plant_mir.txt', 'r') as f:
+with open('my_results/plant_mir.txt', 'r') as f:
     lines = f.readlines()
     for line in lines:
         list = line.split(": ")
@@ -31,8 +31,8 @@ def sort_dict_by_values(input_dict):
     return sorted_dict
 
 
-miR_list = plant_mir["Daucus carota"].split(",")
-paper_list = plant_papers["Daucus carota"].split(",")
+miR_list = plant_mir["Solanum lycopersicum"].split(",")
+paper_list = plant_papers["Solanum lycopersicum"].split(",")
 counts = {}
 for item in miR_list:
     item = item.strip("\n")
@@ -47,8 +47,8 @@ for i in counts:
     sum = sum + counts[i]
 
 
-with open('myresults/carrot.txt', 'w', encoding="utf-8") as f:
-        f.write("Daucus carota")
+with open('examples/tomato.txt', 'w', encoding="utf-8") as f:
+        f.write("Solanum lycopersicum")
         f.write(": " + "\n")
         f.write("Summary: counts of miRs " + str(len(miR_list)) + ", counts of papers " +str(len(paper_list)))
         f.write("\n")
@@ -56,6 +56,6 @@ with open('myresults/carrot.txt', 'w', encoding="utf-8") as f:
         f.write("\n")
         f.write(json.dumps(counts))
         f.write("\n")
-        f.write(plant_mir["Daucus carota"])
+        f.write(plant_mir["Solanum lycopersicum"])
         f.write('\n')
-        f.write(plant_papers["Daucus carota"])
+        f.write(plant_papers["Solanum lycopersicum"])
