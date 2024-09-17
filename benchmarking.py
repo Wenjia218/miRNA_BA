e6274bf5e2f267d9eb784f6ca907e1830414b96d
Wenjia <zhongwenjia218@gmail.com>
2024-09-17 21:28:15 +0200

    commit

--- benchmarking.py
+++ benchmarking.py
@@ -1,3 +1,6 @@
+# structure of csv
+# PMCID, Value, if tax in topic/abstract, if tax > 3, if FORM in topic/abstract, if FORM > 3 times, all found syns and positions.....
+
 plant_papers = {}
 filename = 'textmining_resutls/unique_tax_itis.txt'
 f = open(filename, encoding="utf-8")
@@ -37,31 +40,69 @@ while True:
         list_PMCIDs.append(PMCID)
     else:
         print("Wrong input")
-        break
 
-output_lines = {}
+all_PMCS = []
+
+plant_lines = {}
+if_tax_topic_abstract = {}
+if_tax_bigger_three = {}
 for PMC in plant_papers:
     for PMC2 in list_PMCIDs:
         if PMC == PMC2:
-            output_lines[PMC] = plant_papers[PMC]
+            all_PMCS.append(PMC)
+            plant_lines[PMC] = plant_papers[PMC]
+            if int(plant_papers[PMC].split(",")[0].split("|")[1][0]) < 3:
+                if_tax_topic_abstract[PMC] = True
+            else:
+                if_tax_topic_abstract[PMC] = False
+            if len(plant_papers[PMC].split(",")) > 3:
+                if_tax_bigger_three[PMC] = True
+            else:
+                if_tax_bigger_three[PMC] = False
 
+
+FORM_lines = {}
+if_FORM_topic_abstract = {}
+if_FORM_bigger_three = {}
 for PMC in FORM_papers:
     for PMC2 in list_PMCIDs:
         if PMC == PMC2:
-            output_lines[PMC] = output_lines[PMC] + "   " + FORM_papers[PMC]
-            print(output_lines[PMC])
-
-
+            if PMC not in all_PMCS:
+                all_PMCS.append(PMC)
+            FORM_lines[PMC] = FORM_papers[PMC]
+            if int(FORM_papers[PMC].split(",")[0].split("|")[1][0]) < 3:
+                if_FORM_topic_abstract[PMC] = True
+            else:
+                if_FORM_topic_abstract[PMC] = False
+            if len(FORM_papers[PMC].split(",")) > 3:
+                if_FORM_bigger_three[PMC] = True
+            else:
+                if_FORM_bigger_three[PMC] = False
 
 import csv
 
 with open('benchmarking.csv', 'w', newline='') as csvfile:
-    spamwriter = csv.writer(csvfile, delimiter=' ',
-                            quotechar='|', quoting=csv.QUOTE_MINIMAL)
-    for PMC in output_lines:
+    spamwriter = csv.writer(csvfile, delimiter=' ')
+    for PMC in all_PMCS:
         write_list = []
         write_list.append(PMC + ",")
-        write_list.append(output_lines[PMC].replace("|", ""))
+        write_list.append("unknown" + ",")
+        if PMC in plant_lines:
+            write_list.append(str(if_tax_topic_abstract[PMC]) + ",")
+            write_list.append(str(if_tax_bigger_three[PMC]) + ",")
+        else:
+            write_list.append("False" + ",")
+            write_list.append("False" + ",")
+        if PMC in FORM_lines:
+            write_list.append(str(if_FORM_topic_abstract[PMC]) + ",")
+            write_list.append(str(if_FORM_bigger_three[PMC]) + ",")
+        else:
+            write_list.append("False" + ",")
+            write_list.append("False" + ",")
+        if PMC in plant_lines:
+            write_list.append(plant_lines[PMC] + ",")
+        if PMC in FORM_lines:
+            write_list.append(FORM_lines[PMC] + ",")
         spamwriter.writerow(write_list)
 
 
